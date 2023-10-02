673e2398d5bb0db2e59b03e2ae5cd14c79a76f1c
joao <joaom.sch@outlook.com>
2023-10-02 14:54:38 -0300

    Module 13 exercise

---
+++ src/com/exercises/modules/thirteen/LegalPerson.java
@@ -0,0 +1,22 @@
+package com.exercises.modules.thirteen;
+
+public class LegalPerson extends Person {
+  private String cnpj;
+  private String corporateName;
+
+  public String getCnpj() {
+    return cnpj;
+  }
+
+  public void setCnpj(String cnpj) {
+    this.cnpj = cnpj;
+  }
+
+  public String getCorporateName() {
+    return corporateName;
+  }
+
+  public void setCorporateName(String corporateName) {
+    this.corporateName = corporateName;
+  }
+}
---
+++ src/com/exercises/modules/thirteen/PhysicalPerson.java
@@ -0,0 +1,31 @@
+package com.exercises.modules.thirteen;
+
+public class PhysicalPerson extends Person {
+  private String cpf;
+  private String gender;
+  private int age;
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+}
